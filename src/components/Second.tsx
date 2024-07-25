4688dffcf016894969d235662347f2c44646ff75
Harsh Raj <128404446+harshxraj@users.noreply.github.com>
2024-07-25 22:04:31 +0530

    Update Second.tsx

--- src/components/Second.tsx
+++ src/components/Second.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
-// const data = [
-//   { id: 53, Month: "Jan", Last_year: 5000, This_year: 6000 },
-//   { id: 54, Month: "Feb", Last_year: 10000, This_year: 2000 },
-//   { id: 55, Month: "Mar", Last_year: 20000, This_year: 40000 },
-//   { id: 56, Month: "Apr", Last_year: 32000, This_year: 21000 },
-//   { id: 57, Month: "May", Last_year: 12000, This_year: 9200 },
-//   { id: 58, Month: "Jun", Last_year: 13000, This_year: 8700 },
-// ];
+const data = [
+  { id: 53, Month: "Jan", Last_year: 5000, This_year: 6000 },
+  { id: 54, Month: "Feb", Last_year: 10000, This_year: 2000 },
+  { id: 55, Month: "Mar", Last_year: 20000, This_year: 40000 },
+  { id: 56, Month: "Apr", Last_year: 32000, This_year: 21000 },
+  { id: 57, Month: "May", Last_year: 12000, This_year: 9200 },
+  { id: 58, Month: "Jun", Last_year: 13000, This_year: 8700 },
+];
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
@@ -24,22 +24,22 @@ type ComparisonItem = {
 };
 
 const Second = () => {
-  const [comparisonItems, setComparisonItems] = useState<ComparisonItem[]>([]);
+  const [comparisonItems, setComparisonItems] = useState<ComparisonItem[]>(data);
 
-  const fetchData = async () => {
-    let { data: comparison_items, error: comparison_items_error } = await supabase
-      .from("comparison")
-      .select("*");
-    console.log("DATA", comparison_items);
-    if (comparison_items_error) console.error("Error fetching data:");
-    else {
-      setComparisonItems(comparison_items || []);
-    }
-  };
+  // const fetchData = async () => {
+  //   let { data: comparison_items, error: comparison_items_error } = await supabase
+  //     .from("comparison")
+  //     .select("*");
+  //   console.log("DATA", comparison_items);
+  //   if (comparison_items_error) console.error("Error fetching data:");
+  //   else {
+  //     setComparisonItems(comparison_items || []);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return (
     <div className="w-full pl-6 bg-white pt-1">
